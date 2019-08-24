7bad4aebd352f4e3bbe357c31b1251b59bf3e075
Tedhoon <gt0305@likelion.org>
2019-08-25 00:36:25 +0900

    routing

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+
+
+//두개의 컴포넌트를 불러와서 한 파일로 내보낼 수 있게 함
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,12 +1,20 @@
-import React , {Component} from 'react';
+import React , { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home , About } from 'pages'
+
+
+
 
 class App extends Component {
     render(){
         return(
             <div>
-                Hello React
+                <Route exact path ="/" component = {Home} />  
+                {/* exact가 붙으면 주어진 경로와 정확하게 맞아 떨어져야 컴포넌트를 보여준다 */}
+                {/* 라우팅과 함께 컴포넌트를 불러준다! 와씨 여기에서 component를 Component로 써주면 안되네 ㅋㅋㅋ */}
+                <Route path = "/about" component = {About} />
             </div>
-        )
+        );
     }
 
 }
